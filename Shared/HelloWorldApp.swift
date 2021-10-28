ae99cd7c5fc6f9fc12703dd0d093cfc0e4fcd8bf
Ljubisic,Bratislav (BI X) BIX-DE-I <bratislav.ljubisic@boehringer-ingelheim.com>
2021-10-28 22:15:31 +0200

    Initial Commit

---
+++ Shared/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  Shared
+//
+//  Created by Bratislav Ljubisic on 28.10.21.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
